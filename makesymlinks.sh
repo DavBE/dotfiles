c036c6a9c962c937d1b3e522c6741edf13666503
DavBE <DavBE@users.noreply.github.com>
2015-08-27 14:48:45 +0200

    Added gitconfig to makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc screenrc"    # list of files/folders to symlink in homedir
+files="bashrc screenrc gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
